eeb36c62203abace4888f9342b6e92e2c6b43c6a
David Goulet-Paradis <1346116@cstjean.qc.ca>
2019-03-19 15:58:03 -0400

    java doc

--- echec/src/echec/Pieces/TestTour.java
+++ echec/src/echec/Pieces/TestTour.java
@@ -1,16 +1,34 @@
 package echec.Pieces;
 
+/**
+ * Test tour
+ *
+ * @author Olivier Chan
+ * @author David Goulet
+ */
 public class TestTour extends TestPiece {
     @Override
     protected Piece creerPiece(Piece.Couleur p_couleur)  {
         return Tour.obtenirPiece(p_couleur);
     }
+
+    /**
+     * Test de la représentation d'une tour
+     */
     public void testRepresentation() {
         assertEquals(Tour.REP_TOUR, super.m_piece.getRepresentation());
     }
+
+    /**
+     * Test le type d'une tour
+     */
     public void testType() {
         assertEquals(Piece.TypePiece.Tour, super.m_piece.getTypePiece());
     }
+
+    /**
+     * Test le pointage d'une tour
+     */
     public void testPointage() {
         assertEquals(Piece.TypePiece.Tour.getPointage(), super.m_piece.getTypePiece().getPointage());
     }
